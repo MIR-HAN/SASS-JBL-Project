49fe252eef8a8e8fa34520f27cf5499fed602cd9
MIR-HAN <m.mirhanyorulmazz@gmail.com>
2023-12-29 16:41:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+<h1> SASS-JBL-Project </h1>
+
+This website is intended to create a simple and user-friendly website using HTML and CSS/SCSS.
+
+<h2> Programming Languages Used</h2>
+
+Website is coded with HTML and CSS/SCSS !
+
+<h2>Screen </h2>
+
+![](Web.gif)
+![](Mobile.gif)
+
---
+++ main.js
@@ -0,0 +1,21 @@
+// show menu
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav__link");
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
